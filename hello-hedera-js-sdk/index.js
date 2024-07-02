9642d9b1af8104f96d8109fa08146e79ee5b82de
Shinya Koyama <koshin801@gmail.com>
2024-07-02 18:04:15 +1000

    Added some projects from tut

--- hello-hedera-js-sdk/index.js
+++ hello-hedera-js-sdk/index.js
@@ -1,73 +1,152 @@
 const {
   Hbar,
   Client,
-  PrivateKey,
-  AccountBalanceQuery,
-  TransferTransaction,
-  AccountCreateTransaction,
+  ContractCreateTransaction,
+  FileCreateTransaction,
+  ContractFunctionParameters,
+  ContractCallQuery,
+  ContractExecuteTransaction,
 } = require("@hashgraph/sdk");
+
 require("dotenv").config();
 
-async function transferHbar() {
-  // Grab your Hedera testnet account ID and private key from your .env file
+async function environmentSetup() {
+  //Grab your Hedera testnet account ID and private key from your .env file
   const myAccountId = process.env.MY_ACCOUNT_ID;
   const myPrivateKey = process.env.MY_PRIVATE_KEY;
 
   // If we weren't able to grab it, we should throw a new error
-  if (myAccountId == null || myPrivateKey == null) {
+  if (!myAccountId || !myPrivateKey) {
     throw new Error(
-      "Environment variables myAccountId and myPrivateKey must be present"
+      "Environment variables MY_ACCOUNT_ID and MY_PRIVATE_KEY must be present"
     );
   }
 
-  // Create your connection to the Hedera network
+  //Create your Hedera Testnet client
   const client = Client.forTestnet();
 
   //Set your account as the client's operator
   client.setOperator(myAccountId, myPrivateKey);
 
-  // Set default max transaction fee & max query payment
-  client.setDefaultMaxTransactionFee(new Hbar(100));
-  client.setDefaultMaxQueryPayment(new Hbar(50));
+  //Import the compiled contract from the HelloHedera.json file
+  let helloHedera = require("./HelloHedera.json");
+  const bytecode = helloHedera.data.bytecode.object;
 
-  // Create new keys
-  const newAccountPrivateKey = PrivateKey.generateED25519();
-  const newAccountPublicKey = newAccountPrivateKey.publicKey;
+  //Create a file on Hedera and store the hex-encoded bytecode
+  const fileCreateTx = new FileCreateTransaction()
+    //Set the bytecode of the contract
+    .setContents(bytecode);
 
-  // Create a new account with 1,000 tinybar starting balance
-  const newAccountTransactionResponse = await new AccountCreateTransaction()
-    .setKey(newAccountPublicKey)
-    .setInitialBalance(Hbar.fromTinybars(1000))
-    .execute(client);
+  //Submit the file to the Hedera test network signing with the transaction fee payer key specified with the client
+  const submitTx = await fileCreateTx.execute(client);
 
-  // Get the new account ID
-  const getReceipt = await newAccountTransactionResponse.getReceipt(client);
-  const newAccountId = await getReceipt.accountId;
+  //Get the receipt of the file create transaction
+  const fileReceipt = await submitTx.getReceipt(client);
 
-  console.log("\nNew account ID: " + newAccountId);
+  //Get the file ID from the receipt
+  const bytecodeFileId = fileReceipt.fileId;
 
-  // Verify the account balance
-  const accountBalance = await new AccountBalanceQuery()
-    .setAccountId(newAccountId)
-    .execute(client);
+  //Log the file ID
+  console.log("The smart contract byte code file ID is " + bytecodeFileId);
 
-  console.log(
-    "\nNew account balance is: " +
-      accountBalance.hbars.toTinybars() +
-      " tinybars."
-  );
+  // Instantiate the contract instance
+  const contractTx = await new ContractCreateTransaction()
+    //Set the file ID of the Hedera file storing the bytecode
+    .setBytecodeFileId(bytecodeFileId)
+    //Set the gas to instantiate the contract
+    .setGas(1000000)
+    //Provide the constructor parameters for the contract
+    .setConstructorParameters(
+      new ContractFunctionParameters().addString("Hello from Hedera!")
+    );
 
-  // Create the transfer transaction
-  const sendHbar = await new TransferTransaction()
-    .addHbarTransfer(myAccountId, Hbar.fromTinybars(-1000))
-    .addHbarTransfer(newAccountId, Hbar.fromTinybars(1000))
-    .execute(client);
+  //Submit the transaction to the Hedera test network
+  const contractResponse = await contractTx.execute(client);
 
-  // Verify the transaction reached consensus
-  const transactionReceipt = await sendHbar.getReceipt(client);
-  console.log(
-    "\nThe transfer transaction from my account to the new account was: " +
-      transactionReceipt.status.toString()
-  );
+  //Get the receipt of the file create transaction
+  const contractReceipt = await contractResponse.getReceipt(client);
+
+  //Get the smart contract ID
+  const newContractId = contractReceipt.contractId;
+
+  //Log the smart contract ID
+  console.log("The smart contract ID is " + newContractId);
+
+  // Calls a function of the smart contract
+  const contractQuery = await new ContractCallQuery()
+    //Set the gas for the query
+    .setGas(1000000)
+    //Set the contract ID to return the request for
+    .setContractId(newContractId)
+    //Set the contract function to call
+    .setFunction("get_message")
+    //Set the query payment for the node returning the request
+    //This value must cover the cost of the request otherwise will fail
+    .setQueryPayment(new Hbar(2));
+
+  //Submit to a Hedera network
+  const getMessage = await contractQuery.execute(client);
+
+  // Get a string from the result at index 0
+  const message = getMessage.getString(0);
+
+  //Log the message
+  console.log("The contract message: " + message);
+
+  //Create the transaction to update the contract message
+  const contractExecTx = await new ContractExecuteTransaction()
+    //Set the ID of the contract
+    .setContractId(newContractId)
+    //Set the gas for the contract call
+    .setGas(100000)
+    //Set the contract function to call
+    .setFunction(
+      "set_message",
+      new ContractFunctionParameters().addString("Hello from Hedera again!")
+    );
+
+  //Submit the transaction to a Hedera network and store the response
+  const submitExecTx = await contractExecTx.execute(client);
+
+  //Get the receipt of the transaction
+  const receipt2 = await submitExecTx.getReceipt(client);
+
+  //Confirm the transaction was executed successfully
+  console.log("The transaction status is " + receipt2.status.toString());
+
+  //Query the contract for the contract message
+  const contractCallQuery = new ContractCallQuery()
+    //Set the ID of the contract to query
+    .setContractId(newContractId)
+    //Set the gas to execute the contract call
+    .setGas(100000)
+    //Set the contract function to call
+    .setFunction("get_message")
+    //Set the query payment for the node returning the request
+    //This value must cover the cost of the request otherwise will fail
+    .setQueryPayment(new Hbar(2));
+
+  //Submit the transaction to a Hedera network
+  const contractUpdateResult = await contractCallQuery.execute(client);
+
+  //Get the updated message at index 0
+  const message2 = contractUpdateResult.getString(0);
+
+  //Log the updated message to the console
+  console.log("The updated contract message: " + message2);
+
+  //v2 Hedera JavaScript SDK
+
+  //v2 Hedera JavaScript SDK
+
+  //v2 JavaScript SDK
+
+  // //Set the default maximum transaction fee (in Hbar)
+  // client.setDefaultMaxTransactionFee(new Hbar(100));
+
+  // //Set the maximum payment for queries (in Hbar)
+  // client.setDefaultMaxQueryPayment(new Hbar(50));
+
+  // console.log("Client setup complete.");
 }
-transferHbar();
+environmentSetup();
